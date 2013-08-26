0eefe05e67674a2c6d15b44310d2cbee3ba35fdf
defact0 <ejungdo@naver.com>
2013-08-26 17:36:09 +0900

    새로운 코드

--- src/exJava/Hello.java
+++ src/exJava/Hello.java
@@ -4,8 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("안녕?");
-		System.out.println("master!");
+		System.out.println("master가 새로운 코드를 추가했다!!");
 	}
 
 }
