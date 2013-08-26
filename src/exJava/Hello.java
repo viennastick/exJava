72dec09efa36af24bfec5baff2cc3843392eab3c
defact0 <ejungdo@naver.com>
2013-08-26 18:59:40 +0900

    aaaa

--- src/exJava/Hello.java
+++ src/exJava/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("참여자가 새로운 코드를 추가!");
+		System.out.println("참여자가 새로운 sssss코드를 추가!");
 	}
 
 }
